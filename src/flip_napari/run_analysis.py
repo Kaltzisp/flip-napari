91d7cc58023091782d2a9f58f44db4457f1a7633
Peter Kaltzis <peterkaltzis@outlook.com>
2024-06-20 06:22:44 +1000

    updated run_analysis.py

--- src/flip_napari/run_analysis.py
+++ src/flip_napari/run_analysis.py
@@ -1,65 +1,77 @@
-import os
-import napari
-import tifffile
 from magicgui import magic_factory
-
-import numpy as np
-import pandas as pd
+from .utils import Timer, Widgets
+import tifffile
+from pandas import DataFrame
 from skimage.measure import regionprops_table
+from os import path
+from math import pi
+import numpy as np
+from napari import current_viewer
+from vispy.color import Colormap
 
 
 @magic_factory(
-    labels_image={"widget_type": "FileEdit", "tooltip": "path to the nuclei label image"},
-    intensity_image={"widget_type": "FileEdit", "tooltip": "path to the intensity (marker) image"},
-    masks_image={"widget_type": "FileEdit", "tooltip": "path to the Amira masks file"},
-    threshold={"widget_type": "FloatSlider", "value": 0.3, "max": 1, "tooltip": "threshold for determining marker activity"},
-    target_tissues={"widget_type": "LineEdit", "value": "", "tooltip": "tissues to include in the created activity label image"},
-    call_button="Run Segmentation"
+    label_image_path=Widgets.FileWidget("label image", "path to the nuclear label image"),
+    intensity_image_path=Widgets.FileWidget("intensity image", "path to the intensity image"),
+    masks_image_path=Widgets.FileWidget("masks image", "path to the masks image created in Amira"),
+    threshold=Widgets.FloatWidget("threshold", "pixel intensity threshold for determining marker activity", 0, 1, 0.05, 0.4),
+    min_diameter=Widgets.TextWidget("min diameter", "minimum permitted diameter for a cell to be included in summary results"),
+    target_tissues=Widgets.TextWidget("target tissues", "indexes of tissues to include in the generated red/blue activity label")
 )
-def run_analysis(labels_image, intensity_image, masks_image, threshold, target_tissues):
-    # Opening images.
-    labels = tifffile.imread(labels_image)
-    marker = tifffile.imread(intensity_image)
-    amira_masks = tifffile.imread(masks_image)
+def run_analysis(label_image_path, intensity_image_path, masks_image_path, threshold, min_diameter, target_tissues):
+    timer = Timer("marker analysis")
 
-    # Getting label props dataframe.
-    df = pd.DataFrame(regionprops_table(labels, intensity_image=marker, properties=["label", "intensity_mean", "centroid", "area"]))
-    df["tissue"] = amira_masks[df["centroid-0"].astype(int), df["centroid-1"].astype(int), df["centroid-2"].astype(int)]
-    df.to_csv(os.path.join(os.path.dirname(labels_image), "data_full.csv"))
+    # Opening images and getting path.
+    label_image = tifffile.imread(label_image_path)
+    intensity_image = tifffile.imread(intensity_image_path)
+    masks_image = tifffile.imread(masks_image_path)
+    save_dir = path.dirname(label_image_path)
 
-    # Collecting summary and average information.
+    # Getting label data.
+    df = DataFrame(regionprops_table(label_image, intensity_image=intensity_image, properties=["label", "intensity_mean", "centroid", "area"]))
+    df["tissue"] = masks_image[df["centroid-0"].astype(int), df["centroid-1"].astype(int), df["centroid-2"].astype(int)]
+    df.to_csv(path.join(save_dir, "data_full.csv"))
+
+    # Thresholding df by minimum volume.
+    min_volume = (4/3) * pi * ((float(min_diameter) / 2) ** 3) if min_diameter else 0
+    df = df[df["area"] >= min_volume]
+
+    # Collecting summary information.
     tissue_indexes = []
-    positives = []
-    totals = []
+    total_objects = []
+    positive_objects = []
     for tissue in df["tissue"].unique():
         tissue_df = df[df["tissue"] == tissue]
         tissue_indexes.append(tissue)
-        positives.append((tissue_df["intensity_mean"] >= 255 * threshold).sum())
-        totals.append(len(tissue_df))
-    result = pd.DataFrame({
+        total_objects.append(len(tissue_df))
+        positive_objects.append((tissue_df["intensity_mean"] >= 255 * threshold).sum())
+    result = DataFrame({
         "tissue": tissue_indexes,
-        "total_objects": totals,
-        "positives": positives
+        "positive": positive_objects,
+        "total": total_objects
     })
-    result.to_csv(os.path.join(os.path.dirname(labels_image), "data_summary.csv"))
+    result.to_csv(path.join(save_dir, "data_summary.csv"))
 
     # Adding to viewer.
-    viewer = napari.current_viewer()
+    viewer = current_viewer()
     viewer.dims.ndisplay = 2
-    viewer.add_image(marker)
-    viewer.add_labels(amira_masks)
-    viewer.add_labels(labels)
+    viewer.add_image(intensity_image)
+    viewer.add_labels(masks_image, opacity=0.5)
 
-    # Creating marker image.
+    # Creating activity label.
     if len(target_tissues) > 0:
         selected_indexes = [int(i) for i in target_tissues.split(",")]
-        marker_image = np.zeros(labels.shape + (3,), dtype=np.uint8)
+        activity_label = np.zeros_like(label_image)
         for label in df.itertuples(index=True):
-            print(str(round(100 * label.Index / len(df), 1)) + " %")
+            print(f"Computing label {label.Index} of {len(df)}.")
             if (label.tissue in selected_indexes):
                 if (label.intensity_mean >= 255 * threshold):
-                    marker_image[labels == label.label] = [200, 0, 0]
+                    activity_label[label_image == label.label] = 2
                 else:
-                    marker_image[labels == label.label] = [0, 0, 200]
-        tifffile.imwrite(os.path.join(os.path.dirname(labels_image), "label_marker.tif"), marker_image)
-        viewer.add_image(marker_image)
+                    activity_label[label_image == label.label] = 1
+        tifffile.imwrite(path.join(save_dir, "label_activity.tif"), activity_label)
+
+        # Creating color map and adding to viewer.
+        viewer.add_image(activity_label, opacity=0.75, colormap=Colormap(["transparent", "blue", "red"]))
+
+    timer.print_duration()
