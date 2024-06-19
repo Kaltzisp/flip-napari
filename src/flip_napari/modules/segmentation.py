af51ca66c4f7e468e5ecfcea9ce93ba03b566c5e
Peter Kaltzis <peterkaltzis@outlook.com>
2024-06-19 10:53:57 +1000

    removed obsolete segmentation code

--- src/flip_napari/modules/segmentation.py
+++ src/flip_napari/modules/segmentation.py
@@ -11,20 +11,3 @@ def get_nuclei_labels(nuclei_image):
     labels, flows, styles, diams = model.eval(nuclei_image, do_3D=True)
     timer.print_duration()
     return labels
-
-def segment_on_mask(label_image, mask):
-    timer = Timer("masking")
-    masked_labels = np.zeros(label_image.shape, dtype=label_image.dtype)
-    label_props = regionprops(label_image)
-    for label in label_props:
-        print(label.label)
-        centroid = tuple(int(coord) for coord in label.centroid)
-        if (mask[centroid] == 255):
-            masked_labels[label_image == label.label] = label.label
-    timer.print_duration()
-    unmasked_labels = label_image - masked_labels
-    return masked_labels, unmasked_labels
-
-def get() -> int:
-    print("HELLO WORLD!")
-    return 5
